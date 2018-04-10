9c8a4ba178e5a4865036e154c1773474de5bc117
SonamJagetia <sonamvjagetia@gmail.com>
2018-04-10 14:34:21 +0530

    learn Event arguments

--- testevent.js
+++ testevent.js
@@ -4,11 +4,10 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter(); //create an object
 
 //Register a listener when event is raise
-emitter.on('messageLogged', function(){
-  console.log('Listener called');
+emitter.on('messageLogged', function(arg){ //  or prefer:- e, eventArg
+  console.log('Listener called', arg);
 });
 
 //emit is use to raise an event
-emitter.emit('messageLogged');
-
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
 // Emit meaning : Making a noise, produce somthing - signalling when event is happend

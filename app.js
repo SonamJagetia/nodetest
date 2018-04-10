224084477f8e4eabf69cb88917f87ea55ceb773a
SonamJagetia <sonamvjagetia@gmail.com>
2018-04-10 12:32:55 +0530

    working with FileSystem module

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 
-const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-console.log('Total Memory: ' + totalMemory);
-
-// template string
-// ES6 / ES2015 : ECMAScript 6
-
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
+const files = fs.readdirSync('./');
+console.log(files);

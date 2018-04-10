0cb6ea4f032ca6ea031dd7908f07ddf54fd828cd
SonamJagetia <sonamvjagetia@gmail.com>
2018-04-10 12:40:50 +0530

    Example of Synchronous and Asynchronous method

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 
 const fs = require('fs');
-
+// Synchronous Method
 const files = fs.readdirSync('./');
 console.log(files);
+
+// asynchronous Method
+fs.readdir('./', function(err, files){
+  if (err) console.log('Error', err);
+  else console.log('Result', files);
+});
